77a20c27a3759381c334e54840b6e363b248e88a
Yasmim Morais <yass.happier@gmail.com>
2019-09-03 11:31:54 -0300

    adicionando arquivos corretos para comçar edição

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>AEBA</title>
+
+	<link rel="stylesheet" href="assets/css/style.css">
+</head>
+<body>
+	
+	
+
+</body>
+</html>
