4e8e606d844a561da6d32e1505cc483c66a8ac69
Yasmim Morais <yass.happier@gmail.com>
2019-09-03 23:41:04 -0300

    atualizando

--- index.php
+++ index.php
@@ -1,14 +1,59 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+	<!-- Meta tag obrigatória -->
 	<meta charset="UTF-8">
-	<title>AEBA</title>
+	<title>AEBA - Associação dos Empresários do Bairro do Alecrim</title>
 
+	<!-- Link para a folha de estilo -->
 	<link rel="stylesheet" href="assets/css/style.css">
+	
+	<!-- Favicon  -->
+	<link rel="icon" href="demo_icon.gif" type="image/gif">
+	
+	<!-- Links das fontes: Roboto e Open Sans -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
 </head>
 <body>
-	
-	
+		<div class="container col-lg-8">
+			<div class="container">	
+				<div class="row">
+					<!-- Sobre -->
+					<div class="col bg-dark">	
+						<h1>Grid Example</h1>
+	  					<p>This example demonstrates a 50%/50% split on small, medium, large and xlarge devices. On extra small devices, it will stack (100% width).</p>
+					</div>	
+					
+				</div>
+			</div>     
 
+			<div class="container">     
+				<div class="row">
+					<!-- Objetivos -->
+				    <div class="col-sm-6 bg-success">
+				        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.<br>
+				        Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+				    </div>
+				    <!-- Missão -->
+				    <div class="col-sm-6 bg-warning">
+				        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto.    
+				    </div>
+				</div>
+			</div>
+
+			<div class="container">	
+				<div class="row">
+					<!-- Por que se tornar um associado? -->
+					<div class="col bg-dark">	
+						<h1>Grid Example</h1>
+	  					<p>This example demonstrates a 50%/50% split on small, medium, large and xlarge devices. On extra small devices, it will stack (100% width).</p>
+					</div>	
+					
+				</div>
+			</div>  
+		</div>
+
+	
 </body>
 </html>
