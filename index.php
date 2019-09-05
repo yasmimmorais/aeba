f4b3e8cfe192ccba8f48b3a96c14986f5067a1d0
Yasmim Morais <yass.happier@gmail.com>
2019-09-04 23:43:01 -0300

    design conheca-nos

--- index.php
+++ index.php
@@ -9,84 +9,103 @@
 	<link rel="stylesheet" href="assets/css/style.css">
 	
 	<!-- Favicon  -->
-	<link rel="ico" href="../aeba/assets/img/favicon.ico" type="image/ico">
+	<link rel="shortcut icon" href="assets/img/favicon_aeba.png">
 	
 	<!-- Links das fontes: Roboto e Open Sans -->
-	<link href="https://fonts.googleapis.com/css?family=Roboto&display=swap" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet">
 </head>
 <body>
-		<div class="container col-lg-8">
-			<div class="container">	
-				<div class="row">
-					<!-- Sobre -->
-					<div class="col bg-dark">	
-						<h2 class="title-default">Sobre</h2>
-	  					<p>A Associação dos Empresários do Bairro do Alecrim - AEBA, foi fundada em 19 de Agosto de 2003.</p>
-	  					<br>
-	  					
-	  					<p>A atual diretoria assumiu em junho de 2019 e é composta pelos seguintes empresários:</p>
-						<ul class="estilo-listagem">
-							<li>Presidente: Pedro Campos de Azevedo, empresa Campos Equipamentos e Refrigeração.</li>
-							<li>Vice-Presidente: Matheus Felipe Martins Feitosa, empresa M Impressões Gráfica Rápida.</li>
-							<li>Primeira Secretária: Marilucia da Cunha Sardinha, empresa Malu & Sardinha.</li>
-							<li>Segundo Secretário: Washington Reis de Almeida, empresa Bazar São Paulo.</li>
-							<li>Primeiro Tesoureiro: Enilvan Rodrigues de Macedo, empresa Casa Nobre.</li>
-							<li>Segundo Tesoureiro – Marcelo Torres da Costa, empresa Metal Super.</li>
-						</ul>
-						<br>
+	<!-- Breadcrumb -->
+	<section>
+		<nav aria-label="breadcrumb">
+		  <ol class="breadcrumb">
+		    <li class="breadcrumb-item"><a href="#">Página Inicial</a></li>
+		    <li class="breadcrumb-item active" aria-current="page">Conheça-nos</li>
+		  </ol>
+		</nav
+	</section>
 
-						<p>Conselho Fiscal:</p>
-						<ul class="estilo-listagem">
-							<li>Ailson Carvalho Feitosa, empresa Shopping das festas.</li>
-							<li>Francisco Derneval de Sá, empresa Lojas Sarmento.</li>
-							<li>Ednildo Soares de Araújo, empresa Calçados Araújo.</li>
-						</ul>
-					</div>	
-					
-				</div>
-			</div>     
+	<!-- Sobre -->
+	<section class="container">
+		<div class="row">
+			<div class="col-lg-8 pt-4 mx-auto">
+				<h2 class="title pt-3 pb-1">Sobre</h2>
+				<div class="row-title mb-3"></div>
+				<p class="text-flow">A Associação dos Empresários do Bairro do Alecrim - AEBA, foi fundada em 19 de Agosto de 2003.</p>
+				<p class="sub-title">A atual diretoria assumiu em junho de 2019 e é composta pelos seguintes empresários:</p>
+				<ul class="estilo-listagem">
+					<li class="text-flow">Presidente: Pedro Campos de Azevedo, empresa Campos Equipamentos e Refrigeração.</li>
+					<li class="text-flow">Vice-Presidente: Matheus Felipe Martins Feitosa, empresa M Impressões Gráfica Rápida.</li>
+					<li class="text-flow">Primeira Secretária: Marilucia da Cunha Sardinha, empresa Malu & Sardinha.</li>
+					<li class="text-flow">Segundo Secretário: Washington Reis de Almeida, empresa Bazar São Paulo.</li>
+					<li class="text-flow">Primeiro Tesoureiro: Enilvan Rodrigues de Macedo, empresa Casa Nobre.</li>
+					<li class="text-flow">Segundo Tesoureiro – Marcelo Torres da Costa, empresa Metal Super.</li>
+				</ul>
 
-			<div class="container">     
-				<div class="row">
-					<!-- Objetivos -->
-				    <div class="col-sm-6 bg-success">
-				    	<h2 class="title-default">Objetivos</h2>
-				        <p>A Associação dos Empresários do Bairro do Alecrim (AEBA), tem como objetivo representar os empresários, fazendo reivindicações e apresentando soluções para sanar os problemas, buscando sempre o bem estar da comunidade e o fortalecimento do bairro.</p>
-				        <p>A Diretoria tem como premissa a prestação de serviços aos associados, realização de parcerias com Instituições de Ensino, Entidades do Sistema S (FECOMÉRCIO, SESC, SENAC e SEBRAE), sempre buscando a valorização do bairro, a geração de empregos e o fortalecimento das empresas.</p>
-				        <p>A AEBA também atua junto aos gestores públicos, implantando políticas públicas que tragam benefícios a toda comunidade e as pessoas que transitam pelo bairro.</p>
-				    </div>
-				    <!-- Missão -->
-				    <div class="col-sm-6 bg-warning">
-				    	<h2 class="title-default">Missão</h2>
-				        <p>Lutar pela organização do Alecrim, para que seja o melhor bairro para trabalhar, comprar e até morar.</p>    
-				    </div>
-				</div>
+				<p class="sub-title">Conselho Fiscal:</p>
+				
+				<ul class="estilo-listagem">
+					<li class="text-flow">Ailson Carvalho Feitosa, empresa Shopping das festas.</li>
+					<li class="text-flow">Francisco Derneval de Sá, empresa Lojas Sarmento.</li>
+					<li class="text-flow">Ednildo Soares de Araújo, empresa Calçados Araújo.</li>
+				</ul>
 			</div>
-
-			<div class="container">	
-				<div class="row">
-					<!-- Por que se tornar um associado? -->
-					<div class="col bg-dark">	
-						<h2 class="title-default">Por que se tornar um associado?</h2>
-	  					<ol>
-	  						<li>Ter uma entidade que represente os interesses dos empresários;</li>
-	  						<li>Diretoria com acesso a todos os gestores públicos e privados que podem contribuir para solução dos problemas do Alecrim;</li>
-	  						<li>Associação que busca no diálogo com os ambulantes, melhorias para a organização do bairro;</li>
-	  						<li>A AEBA vem atuando constantemente junto à classe política e gestores públicos, buscando apoio para solucionar os problemas do bairro, sem politizar a associação;</li>
-	  						<li>Associação com transparência na prestação de contas e suas ações;</li>
-	  						<li>Parceria com a Fecomércio, Senac, Sesc e outras entidades não governamentais, para desenvolver ações que irão trazer benefícios aos lojistas e comerciários;</li>
-	  						<li>Criação do Programa AMIGO DO ALECRIM, um projeto que tem como objetivo homenagear os responsáveis que mais contribuírem pela organização e desenvolvimento do Alecrim;</li>
-	  						<li>Ter um site que promove sua empresa, informando os produtos que comercializa e ações promocionais, fortalecendo seus negócios;</li>
-	  						<li>Ter um site que divulga os eventos, notícias e ações da AEBA.</li>
-	  						<li>Associação que promove a interação dos seus associados, através de encontros permanentes, em reunião-almoço e eventos sociais.</li>
-	  					</ol>
-					</div>	
-					
-				</div>
-			</div>  
 		</div>
+	</section>
 
+	<!-- Objetivos -->
+	<section class="container">
+		<div class="row">
+			<div class="col-lg-8 pt-4 mx-auto">
+				<h2 class="title pt-3 pb-1">Objetivos</h2>
+				<div class="row-title mb-3"></div>
+		        <p class="text-flow">A Associação dos Empresários do Bairro do Alecrim (AEBA), tem como objetivo representar os empresários, fazendo reivindicações e apresentando soluções para sanar os problemas, buscando sempre o bem estar da comunidade e o fortalecimento do bairro.</p>
+		        <p class="text-flow">A Diretoria tem como premissa a prestação de serviços aos associados, realização de parcerias com Instituições de Ensino, Entidades do Sistema S (FECOMÉRCIO, SESC, SENAC e SEBRAE), sempre buscando a valorização do bairro, a geração de empregos e o fortalecimento das empresas.</p>
+		        <p class="text-flow">A AEBA também atua junto aos gestores públicos, implantando políticas públicas que tragam benefícios a toda comunidade e as pessoas que transitam pelo bairro.</p>
+			</div>
+		</div>
+	</section>
+
+	<!-- Missão -->
+	<section class="container">
+		<div class="row">
+			<div class="col-lg-8 pt-4 mx-auto">
+				<h2 class="title pt-3 pb-1">Missão</h2>
+				<div class="row-title mb-3"></div>
+				<p class="text-flow">Lutar pela organização do Alecrim, para que seja o melhor bairro para trabalhar, comprar e até morar.</p>
+			</div>
+		</div>
+	</section>
+
+	<!-- Por que se tornar um associado? -->
+	<section class="container">
+		<div class="row">
+			<div class="col-lg-8 pt-4 mx-auto">
+				<h2 class="title pt-3 pb-1">Por que se tornar um associado?</h2>
+				<div class="row-title mb-3"></div>
+					<ol>
+						<li class="text-flow">Ter uma entidade que represente os interesses dos empresários;</li>
+						<li class="text-flow">Diretoria com acesso a todos os gestores públicos e privados que podem contribuir para solução dos problemas do Alecrim;</li>
+						<li class="text-flow">Associação que busca no diálogo com os ambulantes, melhorias para a organização do bairro;</li>
+						<li class="text-flow">A AEBA vem atuando constantemente junto à classe política e gestores públicos, buscando apoio para solucionar os problemas do bairro, sem politizar a associação;</li>
+						<li class="text-flow">Associação com transparência na prestação de contas e suas ações;</li>
+						<li class="text-flow">Parceria com a Fecomércio, Senac, Sesc e outras entidades não governamentais, para desenvolver ações que irão trazer benefícios aos lojistas e comerciários;</li>
+						<li class="text-flow">Criação do Programa AMIGO DO ALECRIM, um projeto que tem como objetivo homenagear os responsáveis que mais contribuírem pela organização e desenvolvimento do Alecrim;</li>
+						<li class="text-flow">Ter um site que promove sua empresa, informando os produtos que comercializa e ações promocionais, fortalecendo seus negócios;</li>
+						<li class="text-flow">Ter um site que divulga os eventos, notícias e ações da AEBA.</li>
+						<li class="text-flow">Associação que promove a interação dos seus associados, através de encontros permanentes, em reunião-almoço e eventos sociais.</li>
+					</ol>
+			</div>
+		</div>
+	</section>
+
+	<!-- Rodapé -->
+	<section class="container">
+		<class class="row">
+			
+		</class>
+	</section>
+			
 	
 </body>
 </html>
