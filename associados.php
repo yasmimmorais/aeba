29175188e030c08b4c7b4d72f2827a2330ba0b81
Yasmim Morais <yass.happier@gmail.com>
2019-09-05 11:19:53 -0300

    novas páginas adicionadas

---
+++ associados.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<!-- Meta tag obrigatória -->
+	<meta charset="UTF-8">
+	<title>AEBA - Associação dos Empresários do Bairro do Alecrim</title>
+
+	<!-- Link para a folha de estilo -->
+	<link rel="stylesheet" href="assets/css/style.css">
+	
+	<!-- Favicon  -->
+	<link rel="shortcut icon" href="assets/img/favicon_aeba.png">
+	
+	<!-- Links das fontes: Roboto e Open Sans -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet">
+</head>
+<body>
+	<!-- Breadcrumb -->
+	<section>
+		<nav aria-label="breadcrumb">
+		  <ol class="breadcrumb">
+		    <li class="breadcrumb-item"><a href="#">Página Inicial</a></li>
+		    <li class="breadcrumb-item active" aria-current="page">Conheça-nos</li>
+		  </ol>
+		</nav>
+	</section>
+
+	<!-- Rodapé -->
+	<section class="container mt-5">
+		<div class="row footer pt-4">
+			<div class="col-sm-12 col-lg-6">
+				<p><img class="icon-footer mr-2" src="assets/img/email.svg" alt="Email">aeba.alecrim@email.com <img class="icon-footer ml-4 mr-2" src="assets/img/whatsapp.svg" alt="Whatsapp">(84) 9 8121 - 4551</p>
+			</div>
+			<div class="col-sm-12 col-lg-6 pb-4">
+				<img class="icon-footer mr-4 float-lg-right" src="assets/img/instagram.svg" alt="instagram">
+				<img class="icon-footer mr-4 sm-mx-auto float-lg-right" src="assets/img/facebook.svg" alt="Facebook">
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="sub-footer col text-center p-3">&copy 2019 - Associação dos Empresários do Bairro do Alecrim. Todos os direitos reservados.</div>
+		</div>
+	</section>
+	
+</body>
+</html>
