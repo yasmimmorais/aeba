e928a84ca09ef36122bb60fd9fb6ea44bde8c46d
Yasmim Morais <yass.happier@gmail.com>
2019-09-06 11:06:54 -0300

    página vizualizar notícia OK

---
+++ visualizar-noticia.php
@@ -0,0 +1,129 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+	<!-- Meta tag obrigatória -->
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="author" content="Yasmim Morais de Vasconcelos">
+	<title>Notícias - Associação dos Empresários do Bairro do Alecrim</title>
+
+	<!-- Link para a folha de estilo -->
+	<link rel="stylesheet" href="assets/css/style.css">
+	
+	<!-- Favicon  -->
+	<link rel="shortcut icon" href="assets/img/favicon_aeba.png">
+	
+	<!-- Links das fontes: Roboto e Open Sans -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet">
+<body>
+	<!-- Breadcrumb -->
+	<section>
+		<nav aria-label="breadcrumb">
+		  <ol class="breadcrumb">
+		    <li class="breadcrumb-item"><a href="#">Página Inicial</a></li>
+		    <li class="breadcrumb-item"><a href="#">Notícias</a></li>
+		    <li class="breadcrumb-item active" aria-current="page">Título da notícia com até tantos dígitos que será definido, que resumer a ideia geral de todo o conteúdo</li>
+		  </ol>
+		</nav>
+	</section>
+	
+	<!-- Título da sessão Notícias --> 
+	<section class="container">
+		<div class="row">
+			<div class="col-lg-8 pt-4 mx-auto">
+				<h2 class="title pt-3 pb-1">Título da notícia com até tantos dígitos</h2>
+			</div>
+		</div>
+	</section>
+	
+	<!-- Descrição da Notícia -->
+	<section class="container">
+		<div class="row">
+			<div class="col-lg-8 mx-auto mt-2">
+				<!-- Descrição da Notícia -->
+		        <p class="description-v-noticia">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras egestas mollis ante, nec sodales quam molesti habitasse platea dictumst. Fusce vitae felis a enim bibendum sodales. Phasellus varius. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+		        <small class="text mb-0">Equipe AEBA - Associação dos Empresários do Bairro do Alecrim</small>
+				<p><small class="text-muted mt-0">Publicado em 05/09/2019</small></p>
+
+				<div class="row-title mb-4"></div>
+			</div>
+			
+			<!-- Imagem da Notícia -->
+			<div class="col-lg-8 mx-auto">
+				<img  class="col-12 p-0" src="assets/img/image-v-noticia.jpg" alt="Imagem da Notícia">
+				<p class="text-center mt-2"><small class="text">Legenda da imagem</small></p>
+			</div>
+		</div>
+
+	</section>
+
+	<!-- Texto da Notícia -->
+	<section class="container">
+		<div class="row">
+			<div class="col-lg-8 pt-4 mx-auto">
+				<p class="text-flow">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras egestas mollis ante, nec sodales quam molestie eget. In fermentum, ligula non sodales posuere, lectus diam gravida urna, vel ultricies orci lacus in magna. In hac habitasse platea dictumst. Fusce vitae felis a enim bibendum sodales. Phasellus varius ante elementum consequat hendrerit. Pellentesque interdum leo et augue euismod, non lacinia lectus varius. Proin euismod porta elit, ut rutrum ante eleifend nec.</p>
+				<p class="text-flow">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus vel pretium nibh. In ac lobortis purus, vel pretium lectus. Nulla id massa in nisl hendrerit euismod eu a erat. Maecenas massa ipsum, mollis vitae dui et, porta volutpat justo. In laoreet volutpat pharetra. Aliquam ut convallis sem. Maecenas vitae ornare nisi. Vestibulum nisl diam, sodales finibus gravida a, gravida eu elit. </p>
+			</div>
+		</div>
+	</section>
+
+	<!-- Ler Mais Notícias -->
+	<section class="container">
+		<div class="row">
+			<div class="col-lg-8 pt-4 mx-auto">
+				<h2 class="title pt-3 pb-1">Ler mais notícias</h2>
+				<div class="row-title mb-3"></div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Cards horizontais de outras notícias -->
+	<section class="container mt-2">
+		<div class="row">
+			<div class="col-lg-8 mx-auto">
+				<div class="card mb-0 border-0">
+				  <div class="row no-gutters ">
+				    <div class="col-md-4">
+				      <img src="assets/img/image-noticia.png" style="width: 100%;" class="" alt="Imagem da Notícia">
+				    </div>
+				    <div class="col-md-8">
+				      <div class="card-body pt-lg-1 pt-sm-3">
+				        <h5 class="card-title title-green">Título da notícia</h5>
+				        <p class="card-text card-description-v-noticia">Aliquam erat volutpat. Aliquam pretium odio non leo semper efficitur. Vivamus rhoncus, sem eu luctus interdum, nibh nunc commodo odio, a auctor lacus ante eget odio. In ac fermentum nulla.</p>
+				        <p class="card-text"><small class="text-muted">Publicado em 05/09/2019</small></p>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<!-- Linha de divisão das notícias		 -->
+			<div class="col-lg-8 mx-auto mt-2">
+				<div class="row-title mb-3"></div>
+			</div>
+		
+			<div class="col-lg-8 mx-auto mt-2">
+				<div class="card mb-0 border-0">
+				  <div class="row no-gutters ">
+				    <div class="col-md-4 ">
+				      <img src="assets/img/image-noticia.png" style="width: 100%;" class="" alt="Imagem da Notícia">
+				    </div>
+				    <div class="col-md-8">
+				      <div class="card-body pt-lg-1 pt-sm-3">
+				        <h5 class="card-title title-green">Título da notícia</h5>
+				        <p class="card-text card-description-v-noticia">Aliquam erat volutpat. Aliquam pretium odio non leo semper efficitur. Vivamus rhoncus, sem eu luctus interdum, nibh nunc commodo odio, a auctor lacus ante eget odio. In ac fermentum nulla.</p>
+				        <p class="card-text"><small class="text-muted">Publicado em 05/09/2019</small></p>
+				      </div>
+				    </div>
+				  </div>
+				</div>
+			</div>
+			<!-- Linha de divisão das notícias		 -->
+			<div class="col-lg-8 mx-auto mt-2">
+				<div class="row-title mb-3"></div>
+			</div>	
+		</div>
+	</section>
+
+</body>
+</html>
