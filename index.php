4ea9c8f403d6221147baf40cecc90ce6f42617ee
Yasmim Morais <yass.happier@gmail.com>
2019-09-08 13:32:15 -0300

    falta ajeitar o funcionamento do menu

--- index.php
+++ index.php
@@ -6,10 +6,6 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="author" content="Yasmim Morais de Vasconcelos">
 	<title>AEBA - Associação dos Empresários do Bairro do Alecrim</title>
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-  	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-
 	<!-- Link para a folha de estilo -->
 	<link rel="stylesheet" href="assets/css/style.css">
 	
@@ -18,63 +14,309 @@
 	
 	<!-- Links das fontes: Roboto e Open Sans -->
 	<link href="https://fonts.googleapis.com/css?family=Open+Sans|Roboto&display=swap" rel="stylesheet">
+	
+	<!-- Link Font Awesome - ícones -->
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 <body>
-
 	<!-- Menu -->
+	<nav class="navbar navbar-expand-lg navbar-light fixed-top bg-white">
+
+	  <button class="navbar-toggler border-0" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Alterna navegação">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
+
+	  <a class="navbar-brand ml-5" href="#">Navbar</a>
+
+	  <div class="collapse navbar-collapse m-3" id="navbarTogglerDemo03">
+	    <ul class="navbar-nav mr-auto mt-2 mt-lg-0 mx-auto">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="#">Início<span class="sr-only">(Página atual)</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Conheça-nos</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Associados</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Produtos/Serviços</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Fale conosco</a>
+	      </li>
+	    </ul>
+
+	    <a href="eita"><button type="submit" class="btn btn-home pl-4 pr-4 mr-5">Área do Associado</button></a>
+	  </div>
+	</nav>
 
 
-	<!-- Carrossel de Imagens -->
-	<section class="container-fluid m-0"> 
-		  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-		    <!-- Indicators -->
-		    <ol class="carousel-indicators">
-		      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-		      <li data-target="#myCarousel" data-slide-to="1"></li>
-		      <li data-target="#myCarousel" data-slide-to="2"></li>
-		    </ol>
+	<!-- Carrossel de divulgações -->
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+	  <ol class="carousel-indicators">
+	    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+	    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+	  </ol>
+	  <div class="carousel-inner">
+	    <div class="carousel-item active">
+	      <img class="d-block w-100" src="assets/img/banner-divulgacao.png" alt="Primeiro Slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="assets/img/banner-divulgacao-2.png" alt="Segundo Slide">
+	    </div>
+	    <div class="carousel-item">
+	      <img class="d-block w-100" src="assets/img/banner-divulgacao.png" alt="Terceiro Slide">
+	    </div>
+	  </div>
+	  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+	    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Anterior</span>
+	  </a>
+	  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+	    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+	    <span class="sr-only">Próximo</span>
+	  </a>
+</div>
 
-		    <!-- Wrapper for slides -->
-		    <div class="carousel-inner">
-		      <div class="item active">
-		        <img src="assets/img/banner-divulgacao.png" alt="Los Angeles" style="width:100%;">
-		      </div>
-		      <div class="item">
-		        <img src="assets/img/banner-divulgacao-2.png" alt="Chicago" style="width:100%;">
-		      </div>
-		    </div>
+	<!-- Barra de pesquisa -->
+	<section class="container">
+		<div class="row"><h4 class="title-pesquisa mx-auto mt-5">O que você está procurando?</h4>
+			<div class="col-12">
 
-		    <!-- Left and right controls -->
-		    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-		      <span class="glyphicon glyphicon-chevron-left"></span>
-		      <span class="sr-only">Previous</span>
-		    </a>
-		    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-		      <span class="glyphicon glyphicon-chevron-right"></span>
-		      <span class="sr-only">Next</span>
-		    </a>
-		  </div>
+				<nav class="navbar navbar-light">
+				  <form class="form-inline mx-auto mt-2 mb-5">
+				    <input class="form-control mr-sm-2" type="search" placeholder="Pesquise aqui" aria-label="Pesquisar">
+				    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Pesquisar</button>
+				  </form>
+				</nav>
+			</div>
+		</div>
 	</section>
 
-	<!-- Rodapé -->
-	<section class="container mt-5">
-		<div class="row footer pt-4">
-			<div class="col-sm-12 col-lg-6">
-				<p> 
-					<img class="icon-footer mr-2" src="assets/img/email.svg" alt="Email">aeba.alecrim@email.com 
-					<img class="icon-footer ml-4 mr-2" src="assets/img/whatsapp.svg" alt="Whatsapp">(84) 9 8121 - 4551
-				</p>
+	<!-- Ícones dos benefícios e suas descrições -->
+	<section class="container-fluid bg-success mt-3 pt-2">
+		<div class="container col-lg-8 col-md-12">
+			<div class="row">
+				<h2 class="bene-title pt-3 pb-1 mx-auto mb-4">Principais Benefícios do Associado</h2>
 			</div>
-			<div class="col-sm-12 col-lg-6 pb-4">
-				<img class="icon-footer mr-4 float-lg-right" src="assets/img/instagram.svg" alt="instagram">
-				<img class="icon-footer mr-4 sm-mx-auto float-lg-right" src="assets/img/facebook.svg" alt="Facebook">
+			<div class="row mx-auto">
+				<div class="text-center mb-3 mr-5 ml-5">
+				<!-- Imagem do produto -->
+				<img src="assets/img/b-1.png" alt="">
+			  	<!-- Descrição do ícone -->
+				<p style="font-family: 'Roboto', sans-serif; font-size: 14px; color: white;">Ações promocionais</p>				  
+				</div>
+
+				<div class="text-center mb-3 mr-5 ml-5">
+				<!-- Imagem do produto -->
+				<img src="assets/img/b-2.png" alt="">
+			  	<!-- Descrição do ícone -->
+				<p style="font-family: 'Roboto', sans-serif; font-size: 14px; color: white;">Treinamentos</p>				  
+				</div>
+
+				<div class="text-center mb-3 mr-5 ml-5">
+				<!-- Imagem do produto -->
+				<img src="assets/img/b-3.png" alt="">
+			  	<!-- Descrição do ícone -->
+				<p style="font-family: 'Roboto', sans-serif; font-size: 14px; color: white;">Participação em<br> eventos</p>				  
+				</div>
+
+				<div class="text-center mb-3 mr-5 ml-5">
+				<!-- Imagem do produto -->
+				<img src="assets/img/b-4.png" alt="">
+			  	<!-- Descrição do ícone -->
+				<p style="font-family: 'Roboto', sans-serif; font-size: 14px; color: white;">Divulgação</p>				  
+				</div>
+
+				<div class="text-center mb-3 mr-5 ml-5">
+				<!-- Imagem do produto -->
+				<img src="assets/img/b-5.png" alt="">
+			  	<!-- Descrição do ícone -->
+				<p style="font-family: 'Roboto', sans-serif; font-size: 14px; color: white;">Networks com<br> empresários</p>				  
+				</div>
+
+				<div class="text-center mb-3 mr-5 ml-5">
+				<!-- Imagem do produto -->
+				<img src="assets/img/b-6.png" alt="">
+			  	<!-- Descrição do ícone -->
+				<p style="font-family: 'Roboto', sans-serif; font-size: 14px; color: white;">Desconto com <br>parceiros<br> conveniados</p>				  
+				</div>
+
+				<div class="text-center mb-3 mr-5 ml-5">
+				<!-- Imagem do produto -->
+				<img src="assets/img/b-7.png" alt="">
+			  	<!-- Descrição do ícone -->
+				<p style="font-family: 'Roboto', sans-serif; font-size: 14px; color: white;">Participação em<br> eventos</p>				  
+				</div>
+
+				<div class="text-center mb-3 mr-5 ml-5">
+				<!-- Imagem do produto -->
+				<img src="assets/img/b-8.png" alt="">
+			  	<!-- Descrição do ícone -->
+				<p style="font-family: 'Roboto', sans-serif; font-size: 14px; color: white;">Descontos em<br> eventos<br> patrocinados</p>				  
+				</div>
+			</div>
+		</div>
+	</section>
+
+	<!-- Sessão de notícias -->
+	<section class="container mt-3">
+		<div class="row">
+			<div class="col-lg-10 pt-4 mx-auto">
+				<h2 class="title pt-3 pb-1">Notícias</h2>
+				<div class="row-title mb-5"></div>
 			</div>
 		</div>
 
 		<div class="row">
+			<div class="col-lg-3 mb-5 col-md-4 offset-lg-1">
+				<!-- Imagem -->
+				<img src="assets/img/image-noticia.png" class="card-img-top rounded-0 border-noticia" alt="...">
+			    <div class="card-body p-0 mt-3">
+			    	<!-- Título -->
+			      <a class="card-title-noticia" href="#">Evento na Assembléia para novas obras</a>
+			      <!-- Descrição -->
+			      <p class="card-text text-description mt-1">Presença dos membros do conselho para realização do feito em prol da população.</p>
+			      <p class="card-text"><small class="text-muted">Publicado em 05/09/2019</small></p>
+			    </div>
+			 </div>			
+			<div class="col-lg-3 mb-5 col-md-4">
+				<!-- Imagem -->
+				<img src="assets/img/image-noticia.png" class="card-img-top rounded-0 border-noticia" alt="...">
+			    <div class="card-body p-0 mt-3">
+			    	<!-- Título -->
+			      <a class="card-title-noticia" href="#">Evento na Assembléia para novas obras</a>
+			      <!-- Descrição -->
+			      <p class="card-text text-description mt-1">Presença dos membros do conselho para realização do feito em prol da população.</p>
+			      <p class="card-text"><small class="text-muted">Publicado em 05/09/2019</small></p>
+			    </div>
+			 </div>
+			<div class="col-lg-3 mb-5 col-md-4">
+				<!-- Imagem -->
+				<img src="assets/img/image-noticia.png" class="card-img-top rounded-0 border-noticia" alt="...">
+			    <div class="card-body p-0 mt-3">
+			    	<!-- Título -->
+			      <a class="card-title-noticia" href="#">Evento na Assembléia para novas obras</a>
+			      <!-- Descrição -->
+			      <p class="card-text text-description mt-1">Presença dos membros do conselho para realização do feito em prol da população.</p>
+			      <p class="card-text"><small class="text-muted">Publicado em 05/09/2019</small></p>
+			    </div>
+			 </div>
+
+			<div class="col-12 text-center">
+				<a href="eita"><button type="submit" class="btn btn-home pl-4 pr-4">Ler mais notícias</button></a>
+			</div>
+		</div>	
+	</section>
+
+	<!-- Sessão de conheça empresas associadas -->
+	<section class="container">
+		<div class="row">
+			<div class="col-lg-10 col-md-12 pt-4 mx-auto mt-3">
+				<h2 class="title pt-3 pb-1">Conheça empresas associadas</h2>
+				<div class="row-title mb-5"></div>		
+			</div>
+		</div>
+		<div class="row">
+			<div class="text-center mx-auto">
+					<img class="logo-empresa m-2" src="assets/img/logo-empresa.png" alt="">
+					<img class="logo-empresa m-2" src="assets/img/logo-empresa.png" alt="">
+					<img class="logo-empresa m-2" src="assets/img/logo-empresa.png" alt="">
+					<img class="logo-empresa m-2" src="assets/img/logo-empresa.png" alt="">
+				</div>
+			<div class="col-12 text-center mt-5">
+				<a href="eita"><button type="submit" class="btn btn-home pl-4 pr-4">Ver todas as empresas</button></a>
+			</div>
+		</div>
+	</section>
+
+
+	<!-- Sessão de formulário para contato -->
+	<section class="container">
+		<div class="row">
+			<!-- Título da sessão -->
+			<div class="mt-5 mx-auto col-lg-10 col-md-12 col-sm-12 p-lg-4 p-sm-2 p-md-4">
+				<h2 class="title pt-3 pb-1">Fale Conosco</h2>
+				<div class="row-title mb-1"></div>
+			</div>
+		</div>
+
+		<!-- Texto esquerdo em relação ao formulário -->
+		<div class="row mx-auto">
+			<div class="col-lg-1 col-md-0 col-sm-0"></div>
+			<div class="col-lg-4 col-sm-12 pr-lg-4 p-sm-4"><br>
+				<div class="title-green mt-sm-2">Podemos te ajudar com algo ou deseja se tornar um associado? </div>
+				<div class="text-flow">
+					<br>
+					<p class="text-flow">Envie-nos uma mensagem por meio deste formulário ou, se preferir, para o email aeba.alecrim@email.com ou whatsapp (84) 9 8121 - 4551.</p> 
+					<br>
+				</div>
+			</div>
+
+			<div class="col-lg-6 col-sm-12">
+				<form>
+					  <div class="form-row">
+					    <div class="form-group col-md-6">
+					    <!-- Nome Completo	 -->
+					      <label class="text-flow for="inputNome">Nome Completo*</label>
+					      <input type="text" class="form-control" id="inputNome" placeholder="Seu Nome Completo">
+					    </div>
+					    <div class="form-group col-md-6">
+					    <!-- E-mail -->
+					      <label class="text-flow for="inputEmail">E-mail*</label>
+					      <input type="email" class="form-control" id="inputEmail" placeholder="seuemail@exemplo.com">
+					    </div>
+					  </div>
+					   <div class="form-row">
+					    <div class="form-group col-md-6">
+					    <!-- Telefone -->
+					      <label class="text-flow for="inputTelefone">Telefone*</label>
+					      <input type="text" class="form-control" id="inputTelefone" placeholder="(XX) X XXXX-XXXX">
+					    </div>
+					    <div class="form-group col-md-6">
+					    <!-- Assunto -->
+					      <label class="text-flow for="inputAssunto">Assunto*</label>
+					      <input type="text" class="form-control" id="inputAssunto" placeholder="Título da Mensagem">
+					    </div>
+					  </div>
+					    <div class="form-group">
+						   <label class="text-flow for="textAreaMensagem">Mensagem*</label>
+						   <textarea class="form-control" id="textAreaMensagem" rows="3"></textarea>
+						</div>
+
+	  				<button type="submit" class="btn btn-l-green pl-4 pr-4 float-lg-right mt-3">Enviar</button>
+				</form>
+			</div>
+
+		<!-- Formulário direito em relação ao texto -->
+
+
+		</div>
+
+	</section>
+
+	<!-- Rodapé -->
+	<section class="container-fluid footer">
+	<div class="container mt-5">
+		<div class="row footer pt-4">
+			<div class="col-sm-12 col-lg-6">
+				<p><img class="icon-footer mr-2" src="assets/img/email.png" alt="Email">aeba.alecrim@email.com<br></p>
+				<p><img class="icon-footer mr-2" src="assets/img/whatsapp.png" alt="Whatsapp">(84) 9 8121 - 4551</p>
+			</div>
+			<div class="col-sm-12 col-lg-6 pb-4">
+				<a href="#"><img class="icon-footer mr-4 float-lg-right" src="assets/img/instagram.png" alt="instagram"></a>
+				<a href=""><img class="icon-footer mr-4 sm-mx-auto float-lg-right" src="assets/img/facebook.png" alt="Facebook"></a>
+			</div>
+		</div>
+	</div>
+	</section>
+	<!-- Sub-footer	 -->
+	<section class="container-fluid sub-footer">
+		<div class="row">
 			<div class="sub-footer col text-center p-3">&copy 2019 - Associação dos Empresários do Bairro do Alecrim. Todos os direitos reservados.</div>
 		</div>
 	</section>
-	
 </body>
 </html>
